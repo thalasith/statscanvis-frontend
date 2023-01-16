b406a074c4988aca07f983adbf621b5f6877be42
Boris Tsao <boris.tsao@gmail.com>
2023-01-15 21:21:43 -0800

    tweaking the chart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,44 +37,66 @@ const Home: NextPage = () => {
 
   const Chart = () => {
     return (
-      <ResponsiveContainer width={700} height={300}>
-        <AreaChart
-          data={data}
-          margin={{
-            top: 10,
-            right: 0,
-            left: 70,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis
-            tickFormatter={(value) =>
-              new Intl.NumberFormat("en-US", {
-                notation: "compact",
-                compactDisplay: "short",
-              }).format(value)
-            }
-          />
-          <Tooltip
-            formatter={(value) => value && value.toLocaleString()}
-            wrapperStyle={{ outline: "none" }}
-          />
-          {chartData.map((item) => {
-            return (
-              <Area
-                type="monotone"
-                name={item.name}
-                dataKey={item.dataKey}
-                stroke={item.fill}
-                fill={item.fill}
-                stackId="1"
-              />
-            );
-          })}
-        </AreaChart>
-      </ResponsiveContainer>
+      <div className="m-2 flex w-full flex-col items-center rounded border border-gray-400 lg:mx-8 lg:w-1/2 lg:py-4">
+        <h1 className="text-2xl font-bold text-gray-700">
+          Employment by Industry
+        </h1>
+
+        <ResponsiveContainer width="95%" height={300}>
+          <AreaChart data={data}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis
+              dataKey="month"
+              interval={1}
+              tickFormatter={(value) => {
+                const months = [
+                  "Jan",
+                  "Feb",
+                  "Mar",
+                  "Apr",
+                  "May",
+                  "Jun",
+                  "Jul",
+                  "Aug",
+                  "Sep",
+                  "Oct",
+                  "Nov",
+                  "Dec",
+                ];
+                const dateArr = value.split(" ");
+                const month = months.indexOf(dateArr[0].slice(0, 3));
+                const year = dateArr[1].slice(-2);
+                return `${months[month]} ${year}`;
+              }}
+            />
+            <YAxis
+              tickFormatter={(value) =>
+                new Intl.NumberFormat("en-US", {
+                  notation: "compact",
+                  compactDisplay: "short",
+                }).format(value)
+              }
+            />
+            <Tooltip
+              formatter={(value) => value && value.toLocaleString()}
+              wrapperStyle={{ outline: "none" }}
+            />
+            <Legend />
+            {chartData.map((item) => {
+              return (
+                <Area
+                  type="monotone"
+                  name={item.name}
+                  dataKey={item.dataKey}
+                  stroke={item.fill}
+                  fill={item.fill}
+                  stackId="1"
+                />
+              );
+            })}
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     );
   };
 
@@ -87,7 +109,10 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center">
         <div>Hi there</div>
-        <div className="w-full">{data && <Chart />}</div>
+        <div className="flex w-full flex-col lg:flex-row">
+          {data && <Chart />}
+          {data && <Chart />}
+        </div>
         {/* </div> */}
       </main>
     </>
